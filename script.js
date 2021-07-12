cbdd179b4805458032859b3f93c116b06ec8a639
tamunaadola <tamoadolashvili@gmail.com>
2021-07-12 17:50:16 +0400

    add html files and script.js , also use in js arrays, objects

---
+++ script.js
@@ -0,0 +1,57 @@
+// TASK 1
+let array = [
+    {
+        name: "tamuna.adola", 
+        age:24
+    },
+    {
+        name: "dato.jije", 
+        age:23 
+    },
+    {
+        name: "tobako.twin",
+        age:23
+    },
+    {
+        name: "sophie.damen",
+        age:24
+    },
+    {
+        name: "toi.toidze",
+        age:22
+    }
+];
+console.log(array);
+
+// TASK 2
+let person = {
+    firstName: "dato", 
+    firstName: "jijelava", 
+    nicknames: ["jije"],
+    age: 23,
+    monthlyIncome: 1000,
+}
+console.log(person);
+
+// TASK 3
+let rect = {
+    width:10,
+    height:5,  
+}
+let area = (10 * 5);
+console.log(area);
+
+// TASK 4 
+let ar = [
+    {
+        x:5,
+        y:2
+    },
+    {
+        x:4,
+        y:3
+    }
+];
+
+let distance = Math.sqrt(Math.pow(ar[0].x - ar[1].x, 2) + Math.pow(ar[0].y - ar[1].y, 2));
+console.log(distance);
